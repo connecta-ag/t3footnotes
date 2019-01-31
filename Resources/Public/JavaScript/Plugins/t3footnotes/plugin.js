6140bc95dc9efab835449dadc02cdcf9cf83a33f
Jochen Rieger <j.rieger@connecta.ag>
2019-01-31 15:22:14 +0100

    [FEATURE] added first working RTE modal dialog

--- Resources/Public/JavaScript/Plugins/t3footnotes/plugin.js
+++ Resources/Public/JavaScript/Plugins/t3footnotes/plugin.js
@@ -14,6 +14,8 @@
  * This is a TYPO3 plugin for rte_ckeditor which provides
  * footnotes functionality.
  *
+ * Based on / developed for CKEditor 4.11
+ *
  * Footnotes can be added in one or multiple RTE instances on a page. The
  * entered footnotes will be collected, linked to
  * of the page and / or as tooltips.
@@ -25,15 +27,91 @@
 
     init: function(editor) {
 
-      // editor.ui.addButton('t3footnotes', {
-      //   label: 'Insert Footnote',
-      //   command: 't3footnotes',
-      //   toolbar: 'insert',
-      //   icon: this.path + 'icons/ckeditor-fa.png',
+      // let's add our button to the RTE and connect its functionality
+      editor.ui.addButton('T3footnotes', {
+        label: 'Insert Footnote', // TODO: add locallang label
+        toolbar: 'insert',
+        command: 'insertFootnote',
+        icon: this.path + 'icons/t3footnotes.png',
+      });
+
+      // adding the insert footnote modal dialog
+      editor.addCommand('insertFootnote',
+          new CKEDITOR.dialogCommand('openInsertFootnoteDialog'));
+
+      CKEDITOR.dialog.add('openInsertFootnoteDialog', function(editor) {
+
+        var abc = 123;
+
+        return {
+
+          title: 'Insert Footnote Dialog', // TODO: add locallang label
+          minWidth: 400,
+          minHeight: 200,
+
+          onShow: function() {
+
+            // alert('hey ho onShow');
+
+          },
+
+          // adding the modal dialog content
+          contents: [{
+
+            id: 'content',
+            label: 'Insert Footnote Content', // TODO: add locallang label
+
+            // adding elements to the dialog
+            elements: [{
+              type: 'text',
+              id: 'footnotetext',
+              label: 'Footnote Text' // TODO: add locallang label
+              // validate: CKEDITOR.dialog.validate.notEmpty('Please enter footnote text') // TODO: add locallang label
+            }]
+          }]
+        };
+      });
+
+      // first working example, simple – without any text input opportunity; TODO: might have to be deleted again, later
+      // editor.addCommand('insertFootnote', {
+      //
+      //   // exec: function (editor) {
+      //   //   console.log('CAG JR: hey! this ist the command "insertFootnote"');
+      //   // }
+      //
+      //   exec: openInsertFootnoteModal
+      //
       // });
+
+      // TODO: see, if it's needed or not anymore, in the end
+      // function openInsertFootnoteModal(editor) {
+      //
+      //   // ...
+      //   var selection = editor.getSelection(),
+      //       content = 'No text selected'; // TODO: add locallang label
+      //
+      //   if (selection && selection.getSelectedText()) {
+      //     content = 'Add footnote content referring to selected text: ' + selection.getSelectedText(); // TODO: add locallang label
+      //   }
+      //
+      //   // TODO: remove again - it's just for showing how to add content to the editor's html
+      //   // var now = new Date();
+      //   // editor.insertHtml( 'The current date and time is: <em>' + now.toString() + '</em>' );
+      //
+      //
+      //   require([
+      //       'TYPO3/CMS/Backend/Modal'
+      //   ], function (Modal) {
+      //     Modal.show(
+      //         'Insert Footnote', // TODO: add locallang label
+      //         content
+      //     );
+      //   });
+      // }
+
       console.log('CAG JR: hey, init t3footnotes here!');
 
-    }
+    },
 
   });
 
