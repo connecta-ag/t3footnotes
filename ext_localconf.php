b9522f91950372858505734cb5919d599384470c
Jochen Rieger <j.rieger@connecta.ag>
2019-05-16 16:48:17 +0200

    [TASK] add dummy ext_localconf + ext_tables
    * for later use maybe?

---
+++ ext_localconf.php
@@ -0,0 +1,19 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+// 2019-05-16 CAG JR TODO: check if this file with dummy example code is of any use at all; if not, let's kick it out again one day
+
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+//    'rte_ckeditor_image', 'setup',
+//    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_image/Configuration/TypoScript/ImageRendering/setup.txt">'
+//);
+
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_fontawesome/Configuration/TSConfig/rte.txt">');
+//
+//// Fontawesome V4
+//$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['defaultFA4'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/DefaultFA4.yaml';
+//$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['fullFA4'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/FullFA4.yaml';
+//// Fontawesome V5
+//$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/DefaultFA5.yaml';
+//$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['full'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/FullFA5.yaml';
+//$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['minimal'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/MinimalFA5.yaml';
---
+++ ext_tables.php
@@ -0,0 +1,13 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+// 2019-05-16 CAG JR TODO: check if this file is of any use at all; if not, let's kick it out again one day
+
+// Register Plugin and namespaces
+//\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+//    'CAG.' . $_EXTKEY,
+//    'CKEditor',
+//    'CKEditor'
+//);
+//// Add Plugin Configs
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'CKEditor');
